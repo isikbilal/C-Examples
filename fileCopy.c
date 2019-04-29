93a14af3066876854d89cd305cd3a2a1c100c4ee
Bilal Işık <bilal_isik@outlook.com>
2019-04-29 12:09:07 +0300

    Update fileCopy.c

--- fileCopy.c
+++ fileCopy.c
@@ -3,6 +3,11 @@
 // Eğer 1 argüman alıyorsa; kopyalanacak dosya ismi, orjinal dosyanın sonuna "_kopyası" ifadesi eklenerek oluşturulacak ve 1. argümandaki dosyanın içeriği bu dosyaya aktarılacaktır. 
 // Eğer 2 argüman alıyorsa; 1. argüman içeriği alınacak dosyanın adını, 2. argüman ise oluşturulacak dosyanın adı olacak ve yine 1. argümandaki dosyanın içeriği bu dosyaya aktarılacaktır.
 
+// This will copy the c program from one file to another.
+// The program can take 1 or 2 arguments.
+// If it takes 1 argument; The file name to be copied will be created by adding "_kopy" to the end of the original file and the contents of the file in argument 1 will be transferred to this file.
+// If it takes 2 arguments; 1. The argument content will be the name of the file to be retrieved, the second argument will be the name of the file to be created, and the contents of the file in argument 1 will be transferred to this file.
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
